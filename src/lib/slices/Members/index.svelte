7ed22995c24518783374d98bc492bb53e16f408d
RayanSp <rayan.spall@hva.nl>
2023-09-22 12:21:41 +0200

    squad a page fixed

--- src/lib/slices/Members/index.svelte
+++ src/lib/slices/Members/index.svelte
@@ -12,27 +12,37 @@
 <section>
 	{#each slice.items as item}
 		<article>
-			<h2>{item.name[0].text}</h2>
-			<img src={item.image.url} alt="" />
-			<p>Go to my <a href={item.link.url}>Github</a>!</p>
+			<h2 class="student-name" >{item.name[0].text}</h2>
+			<center><img src={item.image.url} alt="" /></center>
+			<p class="student-name">Go to my <a href={item.link.url}>Github</a>!</p>
 		</article>
 	{/each}
 </section>
 
 <style>
+
 	section {
-		flex-direction: column;
 		display: flex;
+		flex-direction: column;
 		gap: 1em;
 	}
 
-	article {
+	.student-name {
 		text-align: center;
-		background-color: pink;
-		width: 16em;
-		height: 100%;
-		padding: 5px;
-		border: 4px solid grey;
+		align-items: center;
+	}
+	article {
+		
+		color: var(--textColor);
+		padding: 20px;
+		margin: 0 auto;
+		margin-bottom:50px;
+		border: var(--borderSize);
+		border-color: var(--borderColor);
+		border-style: solid;
+		box-shadow: 10px 15px 20px #ff0000;
+		width: 15rem;
+		height: 300px;
 	}
 
 	article img {
